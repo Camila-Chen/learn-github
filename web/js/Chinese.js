11ba9d18e61a3f55e76c83c50d75a83bac3891ec
candyST <455498620@qq.com>
2019-06-12 15:24:48 +0800

    first commit

---
+++ web/js/Chinese.js
@@ -0,0 +1,94 @@
+function toggleBox(index) {
+    currentIndex = index
+    var box = ".box" + index;
+    var change = index;
+    var point = '.point' + change
+    $(box).show();
+
+
+    $('.point-t').removeClass('point-on');
+
+    $(point).addClass('point-on');
+    // hide
+    var numbers = [1, 2, 3];
+    numbers.forEach(myFunction);
+    function myFunction(i) {
+        if (i != index) {
+            var b = ".box" + i
+            $(b).hide()
+        }
+    }
+
+}
+
+
+$(document).ready(function () {
+
+
+    $('.point1').addClass('point-on');
+
+    $(".box2").hide();
+    $(".box3").hide();
+    $(".point-wrap1").click(function () {
+        clear()
+        toggleBox(1)
+        recover()
+    });
+    $(".point-wrap2").click(function () {
+        clear()
+        toggleBox(2)
+        recover()
+    });
+    $(".point-wrap3").click(function () {
+        clear()
+        toggleBox(3)
+        recover()
+    });
+
+});
+
+// $("point").css("background-color", "white");
+
+
+//定义计时器
+var currentIndex = 1
+var timer = setInterval(function () {
+    currentIndex = currentIndex + 1
+    if (currentIndex > 3) {
+        currentIndex = 1
+    }
+    toggleBox(currentIndex)
+}, 2000);
+
+
+
+
+//清除计时器函数
+function clear() {
+    window.clearInterval(timer);
+}
+//恢复计时器函数
+function recover() {
+    timer = setInterval(function () {
+        currentIndex = currentIndex + 1
+        if (currentIndex > 3) {
+            currentIndex = 1
+        }
+        toggleBox(currentIndex)
+    }, 2000);
+    return timer
+}
+
+
+function change1() {
+    window.location = "file:///Users/chentian/Documents/projects/web/code/Chinese.html"
+    document.cookie = "language=Chinese";
+    console.log(document.cookie)
+
+}
+function change2() {
+    window.location = "file:///Users/chentian/Documents/projects/web/code/English.html"
+    document.cookie = "language=English";
+    console.log(document.cookie)
+}
+
---
+++ web/js/variable.js
@@ -0,0 +1,24 @@
+//变量的声明 声明保护用户名的变量
+// var name_01;
+//变量的赋值
+// name_01 = "marry";
+//声明的同时赋值
+// var age = 18;
+//声明保存邮箱的变量
+// var email = "marry@sina.com"
+
+//一次声明多个变量
+var name_01 = "marry", age = 18, email = "marry@sina.com", address;
+//在控制台中打印
+console.log(name_01)
+var distance = 12.67980;
+
+//算数操作符
+var num1 = 10,
+    num2 = 20;
+console.log(num1 * num2);
+//三元操作符
+var soce = 85;
+var result = (soce >= 60) ? "及格" : "不及格";
+console.log(result);
+
